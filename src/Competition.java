2632cbf9bf661ec421e8f30166ed59f7e2fca801
Mani-Yazdankhah <mani.yazdankhah@accolitedigital.com>
2022-11-28 12:45:25 +0000

    added code

--- src/Competition.java
+++ src/Competition.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Competition {
+import java.util.Arrays;
+
+public class Competition {
+    public static int[] votes = new int[]{0, 0, 0}; //Voting box
+
+    public static void main(String[] args) {
+        long timeToVote = 3000; //How long can people vote for (in mS)
+        long start = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - start) < timeToVote) { // Keep receiving votes while there is time
+            new Thread(new Viewer()).start();
+        }
+        System.out.println(Arrays.toString(votes)); // Print out the results
+    }
 }
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Viewer {
+import java.util.Random;
+
+public class Viewer implements Runnable {
+    @Override
+    public void run() {
+        int candidate = new Random().nextInt(3); // Pick a candidate to vote for
+        synchronized (Competition.votes) { // Obtain the lock while voting
+            Competition.votes[candidate]++; // Vote
+        } // Release the lock
+    }
 }
